6afa7440094488505f1e970a37d341d20adc9b1d
Adam Wolff <adam@reginald>
2024-05-08 08:32:40 -0700

    It works

--- ha/views.py
+++ ha/views.py
@@ -5,25 +5,53 @@ import os
 from settings import HA_HOST
 import logging
 import psycopg2
+import httpx
+import asyncio
 
 logger = logging.getLogger(__name__)
 
 ha_api_url = f"http://{HA_HOST}:8123/api"
 ha_access_token = os.environ["HA_ACCESS_TOKEN"]
-conn = psycopg2.connect("dbname=monitoring user=adam host=spine password=adam")
 
-def dashboard(request):
-    data =  latest_air()
-    return render(request, "dashboard.html", data)
+async def dashboard(request):
+    ha_data = await ha_info()
+    print(ha_data)
+    air_data =  latest_air()
+    return render(request, "dashboard.html", ha_data | air_data)
+
+async def ha_info():
+    headers = {
+        "Authorization": f"Bearer {ha_access_token}",
+        "Content-Type": "application/json"
+    }
+
+    def convert_switch_state(switch_state):
+        assert switch_state in ["on", "off"]
+        return switch_state == "on"
+
+    async with httpx.AsyncClient() as client:
+        urls = [
+            ha_api_url + "/states/switch.tidbyt_switch_2", 
+            ha_api_url + "/states/switch.space_heater_switch_2",
+            ha_api_url + "/states/sensor.space_heater_power_2",
+        ]
+        responses = await asyncio.gather(*[client.get(url, headers=headers) for url in urls])
+        t_switch, h_switch, h_power = [response.json()["state"] for response in responses]
+        return {
+            "tidbyt_switch": convert_switch_state(t_switch),
+            "heat_switch": convert_switch_state(h_switch),
+            "heat_power": h_power,
+        }
 
 def latest_air():
-    with conn.cursor() as cur:
-        cur.execute(
-            "select co2, temperature, humidity from air order by time desc limit 1;"
-        )
-        result = cur.fetchone()
-        assert result
-        co2, celsius, humidity = result
+    with psycopg2.connect("dbname=monitoring user=adam host=spine password=adam") as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "select co2, temperature, humidity from air order by time desc limit 1;"
+            )
+            result = cur.fetchone()
+            assert result
+            co2, celsius, humidity = result
 
     farenheight = celsius *  9/5 + 32
     return {
