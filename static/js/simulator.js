7c255cf0f679b0caceaa70430e706e2d06a34170
Adam Wolff <adam@reginald>
2024-05-12 08:57:25 -0700

    Simulator enhancements

---
+++ static/js/simulator.js
@@ -0,0 +1,61 @@
+const container = document.getElementById('container');
+
+function loadWebPImage() {
+  return new Promise((resolve, reject) => {
+    const webpImage = new Image();
+    webpImage.width = 640
+    webpImage.height = 320
+    webpImage.onload = () => {
+      resolve(webpImage);
+    };
+    webpImage.onerror = (error) => {
+      reject(error);
+    };
+    webpImage.src = '/pushbyt/v1/preview.webp'
+  });
+}
+
+function replaceImage(img) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  container.appendChild(img);
+  console.log("Placed img")
+  prepNextImage()
+}
+
+function prepNextImage() {
+  let nextImage = null
+  setTimeout(() => {
+    console.log("Load next")
+    loadWebPImage()
+      .then((webpImage) => {
+        nextImage = webpImage
+      })
+  }, 10000)
+  setTimeout(() => replaceImage(nextImage), 15000)
+}
+
+// Usage
+loadWebPImage()
+  .then((webpImage) => {
+    // WebP image loaded successfully
+    console.log('WebP image loaded');
+    // Append the image to the DOM or perform any other actions
+    replaceImage(webpImage)
+  })
+  .catch((error) => {
+    // Error occurred while loading the WebP image
+    console.error('Error loading WebP image:', error);
+  });
+
+function generate() {
+  fetch("/pushbyt/command/generate")
+    .then(response => response.text())
+    .then(text =>
+      console.log("Generation result: " + text)
+    )
+}
+
+generate()
+setInterval(generate, 60000)
