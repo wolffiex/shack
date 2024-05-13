47e042f00b7a355cbafdf5f07bdf018c41b97214
Adam Wolff <adam@reginald>
2024-05-13 14:43:47 -0700

    Favicon

--- urls.py
+++ urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.templatetags.static import static
 
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=static('favicon.ico'), permanent=True)),
     path("shack/admin/", admin.site.urls),
     path("pushbyt/", include("pushbyt.urls")),
     path("ha/", include("ha.urls")),
