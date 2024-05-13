ecd4512526fc9b51603f3f42f4a6bc9dbb7b1eef
Adam Wolff <adam@reginald>
2024-05-13 14:19:55 -0700

    Don't cache preview

--- pushbyt/views/preview.py
+++ pushbyt/views/preview.py
@@ -1,11 +1,13 @@
 from django.utils import timezone
 from django.shortcuts import redirect
 from pushbyt.models import Animation
+from django.views.decorators.cache import never_cache
 import logging
 
 logger = logging.getLogger(__name__)
 
 
+@never_cache
 def get_preview(_):
     now = timezone.now()
     anim = Animation.get_next_animation(now)
