1c14ca8af5bfdbebac43578e4753d4202c1adfa4
Adam Wolff <awolff@gmail.com>
2024-05-21 09:51:45 -0700

    Timer choose algo

--- pushbyt/views/preview.py
+++ pushbyt/views/preview.py
@@ -37,7 +37,9 @@ def time_str(maybe_time):
 
 def choose_anim(anims, now):
     logger.info(f"Choose anim at {time_str(now)} from list of {len(anims)}")
-    anims.sort(key=cmp_to_key(lambda a1, a2: compare_animations(a1, a2, now)))
+    summary = summarize_anims(anims, now)
+    anims.sort(key=cmp_to_key(
+        lambda a1, a2: compare_animations(a1, a2, summary)))
     for anim in anims:
         sa = time_str(anim.served_at)
         st = time_str(anim.start_time)
@@ -57,8 +59,18 @@ def get_animation_list(now):
     )
 
 
+def summarize_anims(anims, now):
+    last_timer = None
+    for anim in [a for a in anims if a.served_at]:
+        if is_timer(anim):
+            last_timer = anim
+    return {
+        "last_timer": last_timer,
+    }
+
+
 def compare_animations(
-    anim1: Animation, anim2: Animation, now: datetime
+    anim1: Animation, anim2: Animation, summary
 ) -> int:
     anims = [anim1, anim2]
     anim1_not_served, anim2_not_served = [not a.served_at for a in anims]
@@ -76,9 +88,32 @@ def compare_animations(
         # Case where they are both doorbell doesn't matter
         return -1 if anim1_door else 1
 
+    anim1_is_important, anim2_is_important = [
+        is_timer(a) and a.metadata["important"]
+        for a in anims
+    ]
+
+    if anim1_is_important != anim2_is_important:
+        return -1 if anim1_is_important else 1
+
     anim1_no_start, anim2_no_start = [not a.start_time for a in anims]
     if anim1_no_start != anim2_no_start:
         return -1 if anim1_no_start else 1
 
-    # todo: choose between rays and timer
+    # If start times don't match then both are not None
+    if anim1.start_time != anim2.start_time:
+        return -1 if anim1.start_time < anim2.start_time else 1
+
+    # If we get here, both anims have start_time which is the same
+    # For now, choose between rays and timer
+    timer_shown_recently = bool(summary["last_timer"])
+    if is_timer(anim1):
+        return -1 if not timer_shown_recently else 1
+    if is_timer(anim2):
+        return 1 if not timer_shown_recently else -1
+
     return 0
+
+
+def is_timer(anim):
+    return anim.source == Animation.Source.TIMER
