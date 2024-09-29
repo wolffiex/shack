00e1f4ee7983bfc1b63f5fb247b0840b72077c32
Adam Wolff <awolff@gmail.com>
2024-09-29 14:28:53 -0700

    alpha channels

--- pushbyt/animation/radar.py
+++ pushbyt/animation/radar.py
@@ -12,41 +12,48 @@ SCALED_WIDTH, SCALED_HEIGHT = SCALE_FACTOR * WIDTH, SCALE_FACTOR * HEIGHT
 def radar(start_time: datetime):
     t = start_time
     font = ImageFont.truetype("./fonts/DepartureMono/DepartureMono-Regular.ttf", 22)
-    hours_px = get_time_pixels(font, "99")
-    mins_px = get_time_pixels(font, "99")
+    hours_img = get_time_img(font, "99")
+    mins_img = get_time_img(font, "99")
     while True:
-        yield draw_second_hand(datetime_to_radian(t), hours_px, mins_px)
+        yield render_frame(datetime_to_radian(t), hours_img, mins_img)
 
         t += FRAME_TIME
 
 
-def get_time_pixels(font, text):
-    image = Image.new("RGB", (WIDTH, HEIGHT), color="black")
+def get_time_img(font, text):
+    image = Image.new("RGBA", (32, 32), color=(0, 0, 0, 0))
     draw = ImageDraw.Draw(image)
     text_position = (0, 0)
-    draw.text(text_position, text, font=font, fill="white")
-    # Get the list of white pixel coordinates
+    draw.text(text_position, text, font=font, fill="gray")
+
+    # Get the list of white pixel coordinates as a generator
     pixels = image.load()
-    white_pixels = [
-        (x, y) for x in range(WIDTH) for y in range(HEIGHT) if pixels[x, y] != (0, 0, 0)
-    ]
+    white_pixels = (
+        (x, y) for x in range(32) for y in range(32) if pixels[x, y] != (0, 0, 0)
+    )
+
+    if not any(white_pixels):
+        return image
 
     # Calculate the minimum and maximum x and y coordinates
-    left = min(x for x, _ in white_pixels)
-    right = max(x for x, _ in white_pixels)
-    top = min(y for _, y in white_pixels)
-    bottom = max(y for _, y in white_pixels)
+    x_coords, y_coords = zip(*white_pixels)
+    left, right = min(x_coords), max(x_coords)
+    top, bottom = min(y_coords), max(y_coords)
 
-    # Calculate the offsets to center the pixels
-    x_off = (WIDTH // 2 - (right - left)) // 2
-    y_off = (HEIGHT - (bottom - top)) // 2
+    # Crop the image based on the bounding box
+    cropped_image = image.crop((left, top, right + 1, bottom + 1))
 
-    # Apply the offsets to the pixel coordinates
-    return [(x - left + x_off, y - top + y_off) for x, y in white_pixels]
+    # Create a new 32x32 image and paste the cropped image in the center
+    result = Image.new("RGBA", (32, 32), color=(0, 0, 0, 0))
+    x_off = (32 - cropped_image.width) // 2
+    y_off = (32 - cropped_image.height) // 2
+    result.paste(cropped_image, (x_off, y_off))
+
+    return result
 
 
-def draw_second_hand(radian, hours_px, mins_px):
-    image = Image.new("RGB", (SCALED_WIDTH, SCALED_HEIGHT), color="black")
+def second_hand_img(radian) -> Image.Image:
+    image = Image.new("RGBA", (SCALED_WIDTH, SCALED_HEIGHT), color="black")
     center_x, center_y = SCALED_WIDTH // 2, SCALED_HEIGHT // 2
     length = 400
 
@@ -54,27 +61,34 @@ def draw_second_hand(radian, hours_px, mins_px):
     end_x = center_x + int(length * math.sin(radian))
     end_y = center_y - int(length * math.cos(radian))
 
-    # Create a new image with a white background
     draw = ImageDraw.Draw(image)
 
-    # Draw the line representing the second hand
-    draw.line((center_x, center_y, end_x, end_y), fill="white", width=SCALE_FACTOR)
+    draw.line((center_x, center_y, end_x, end_y), fill="yellow", width=SCALE_FACTOR)
 
-    draw = ImageDraw.Draw(image)
-    finimage = image.resize((WIDTH, HEIGHT), resample=Image.LANCZOS)
-    draw = ImageDraw.Draw(finimage)
+    image = image.resize((WIDTH, HEIGHT), resample=Image.LANCZOS)
+    alpha = image.convert("L")
+    result = Image.new("RGBA", image.size)
+    result.paste(image)
+    result.putalpha(alpha)
 
-    for x, y in hours_px:
-        draw.point((x, y), fill="white")
-    for x, y in mins_px:
-        draw.point((WIDTH // 2 + x, y), fill="white")
+    return result
 
+
+def render_frame(radian, hours_img, mins_img):
+    time_image = Image.new("RGBA", (WIDTH, HEIGHT), color=(0, 0, 0, 0))
+    time_image.paste(hours_img, box=(0, 0))
+    time_image.paste(mins_img, box=(32, 0))
+    # return time_image
+    ray_image = second_hand_img(radian)
+    img = Image.alpha_composite(time_image, ray_image)
+    # img = ray_image
+    draw = ImageDraw.Draw(img)
     for x in range(32, 33):
         for y in range(11, 13):
             draw.point((x, y), fill="white")
         for y in range(19, 21):
             draw.point((x, y), fill="white")
-    return finimage
+    return img.convert("RGB")
 
 
 def datetime_to_radian(t):
