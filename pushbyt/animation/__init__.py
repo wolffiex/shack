ca2d2076d1d2d1e101bf6c658d2b23dbed27bf81
Adam Wolff <adam@reginald>
2024-10-12 18:51:16 -0700

    Refactor management command

--- pushbyt/animation/__init__.py
+++ pushbyt/animation/__init__.py
@@ -1,5 +1,5 @@
 from .util import render, FRAME_TIME
 from .generate import generate, update_timer
-from .radar import radar
+from .radar import clock_radar
 
-__all__ = ["render", "FRAME_TIME", "generate", "update_timer", "radar"]
+__all__ = ["render", "FRAME_TIME", "generate", "update_timer", "clock_radar"]
--- pushbyt/management/commands/radar.py
+++ pushbyt/management/commands/radar.py
@@ -1,6 +1,6 @@
 from django_rich.management import RichCommand
-from pushbyt.animation import radar
-from pushbyt.animation import render
+from pushbyt.animation import clock_radar
+from pushbyt.animation import FRAME_TIME, render
 from pathlib import Path
 from itertools import islice
 from datetime import datetime
@@ -15,8 +15,15 @@ class Command(RichCommand):
         self.console.print(self.generate())
 
     def generate(self):
-        now = datetime.now()
-        frames = islice(radar(now), 0, 600)
+        t = datetime.now()
+        frames = []
+        radar = clock_radar()
+        next(radar)  # Prime the generator
+        
+        for _ in range(600):
+            frame = radar.send(t)
+            frames.append(frame)
+            t += FRAME_TIME
         file_path = (Path("render") / "radar-test").with_suffix(".webp")
         render(frames, file_path)
         return file_path.resolve().as_posix()
